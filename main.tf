251184cd6b3b87cb78bad1f2f9df5cde07ac0583
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-03-04 11:27:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,3 @@ provider "google" {
 variable "sa_key" {
   default =  ""
 }
-resource "google_sql_database_instance" "main" {
-  name             = "example-instance-march"
-  database_version = "MYSQL_5_7"
- region           = "us-central1"
-  project          = "your-project-id"
-
-  settings {
-    tier = "db-f1-micro"
-
-    # Enable SSL
-    #ip_configuration {
-    #  require_ssl = true
-    #}
-
-  }
-}
